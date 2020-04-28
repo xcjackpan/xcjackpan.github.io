4230a9de152462213efc4d8ca6364847d8b830b1
xcjackpan <xcjackpan99@gmail.com>
2020-04-28 14:11:19 -0400

    build files

---
+++ precache-manifest.79c75d8903f3f0e98c8661b300c09ef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c0931e18ec945415b2d0a69dab1356",
+    "url": "/xcjackpan.github.io/index.html"
+  },
+  {
+    "revision": "8a0b64d9332c635a001b",
+    "url": "/xcjackpan.github.io/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "a31b6e7264a5db95230a",
+    "url": "/xcjackpan.github.io/static/css/main.c60b7f59.chunk.css"
+  },
+  {
+    "revision": "8a0b64d9332c635a001b",
+    "url": "/xcjackpan.github.io/static/js/2.5b6c4d62.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/xcjackpan.github.io/static/js/2.5b6c4d62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a31b6e7264a5db95230a",
+    "url": "/xcjackpan.github.io/static/js/main.754e9d12.chunk.js"
+  },
+  {
+    "revision": "b3f534cc4605ff390095",
+    "url": "/xcjackpan.github.io/static/js/runtime-main.dc6fd9f7.js"
+  }
+]);
